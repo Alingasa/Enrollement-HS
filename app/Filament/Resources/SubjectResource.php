e56931e8529afbd2eba750999dd7086cff0183fb
Alingasa <jamespagiwayanalingasa@gmail.com>
2024-06-11 18:51:49 +0800

    creating project

---
+++ app/Filament/Resources/SubjectResource.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\CategoryResource\RelationManagers\SubjectsRelationManager;
+use App\Filament\Resources\SubjectResource\Pages;
+use App\Filament\Resources\SubjectResource\RelationManagers;
+use App\Models\Subject;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class SubjectResource extends Resource
+{
+    protected static ?string $model = Subject::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-book-open';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\select::make('section_id')
+                    ->required()
+                    ->relationship(name: 'section', titleAttribute: 'name'),
+                Forms\Components\TextInput::make('subject_code')
+                    ->required(),
+                Forms\Components\TextInput::make('subject_title')
+                    ->required(),
+                Forms\Components\select::make('strand_id')
+                    ->required()
+                    ->relationship(name: 'strand', titleAttribute: 'name'),
+                Forms\Components\TextInput::make('subject_type')
+                    ->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('section.name')
+                    ->searchable()
+                    ->badge()
+                    ->color('primary')
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('strand.name')
+                    ->sortable()
+                    ->badge()
+                    ->color('warning'),
+                Tables\Columns\TextColumn::make('subject_code')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('subject_title')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('subject_type')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('deleted_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                Tables\Filters\TrashedFilter::make(),
+            ])
+            ->actions([
+                Tables\Actions\ViewAction::make(),
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                    Tables\Actions\ForceDeleteBulkAction::make(),
+                    Tables\Actions\RestoreBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListSubjects::route('/'),
+            'create' => Pages\CreateSubject::route('/create'),
+            'view' => Pages\ViewSubject::route('/{record}'),
+            'edit' => Pages\EditSubject::route('/{record}/edit'),
+        ];
+    }
+
+    public static function getEloquentQuery(): Builder
+    {
+        return parent::getEloquentQuery()
+            ->withoutGlobalScopes([
+                SoftDeletingScope::class,
+            ]);
+    }
+}
---
+++ app/Models/Student.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Models;
+
+use App\CivilStatus;
+use App\GenderType;
+use App\GradeType;
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Student extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $guarded = [];
+
+    // protected $with = ['user'];
+
+    protected $appends = ['full_name', 'age'];
+
+    protected function casts()
+    {
+        return [
+            'grade_level' => GradeType::class,
+            'civil_status' => CivilStatus::class,
+            'gender'       => GenderType::class,
+        ];
+    }
+    public function getAgeAttribute()
+    {
+        return Carbon::parse($this->birthdate)->age;
+    }
+
+    public function getFullNameAttribute()
+    {
+        $fullName = "{$this->last_name}, {$this->first_name}";
+        if (!empty($this->middle_name)) {
+            $fullName .= " {$this->middle_name[0]}.";
+        }
+        return $fullName;
+    }
+
+    public function strand(){
+      return  $this->belongsTo(Strand::class);
+    }
+    // public function setSchoolIdAttribute(){
+    //     $currentYear = Carbon::now()->year;
+    //     $lastTwoDigits = substr($currentYear, -2);
+
+    //     return $this->attributes['school_id'] = $lastTwoDigits.'-'.str_pad(Student::count(), 4, '0', STR_PAD_LEFT);
+    // }
+}
+//24-0001
